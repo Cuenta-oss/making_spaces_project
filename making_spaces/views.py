438b2b66b2b664422bd90fc8aa935ae5d1cbaaf0
[Cuenta-oss] <[ejmenrique97@gmail.com]>
2022-10-18 11:22:18 -0500

    custom a templates

--- making_spaces/views.py
+++ making_spaces/views.py
@@ -1,8 +1,30 @@
+from re import template
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
     template_name = 'index.html'
     return render(request, template_name)
+
+def about(request):
+    template_name = 'about.html'
+    return render(request, template_name)
+
+def services(request):
+    template_name = 'services.html'
+    return render(request, template_name)
+
+def projects(request):
+    template_name = 'projects.html'
+    return render(request, template_name)
+
+def blog(request):
+    template_name = 'blog.html'
+    return render(request, template_name)
+
+def contact(request):
+    template_name = 'contact.html'
+    return render(request, template_name)
+
 
 
